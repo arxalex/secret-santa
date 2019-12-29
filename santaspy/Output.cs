8d9e7486be962fef82d106d523392a07f60d6cd7
Oleksandr Yushchenko <arxalex@arxalex.com>
2019-12-29 12:00:00 +0200

    init

---
+++ santaspy/Output.cs
@@ -0,0 +1,603 @@
+﻿using System;
+
+public class Output
+{
+    public static void Matrix(string[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < ar[i, j].Length)
+                        {
+                            len = ar[i, j].Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(int[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < Convert.ToString(ar[i, j]).Length)
+                        {
+                            len = Convert.ToString(ar[i, j]).Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(double[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < Convert.ToString(ar[i, j]).Length)
+                        {
+                            len = Convert.ToString(ar[i, j]).Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(decimal[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < Convert.ToString(ar[i, j]).Length)
+                        {
+                            len = Convert.ToString(ar[i, j]).Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(char[,] ar)
+    {
+        try
+        {
+            {
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * 2;
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(float[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < Convert.ToString(ar[i, j]).Length)
+                        {
+                            len = Convert.ToString(ar[i, j]).Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Matrix(byte[,] ar)
+    {
+        try
+        {
+            {
+                int len = 0;
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        if (len < Convert.ToString(ar[i, j]).Length)
+                        {
+                            len = Convert.ToString(ar[i, j]).Length;
+                        }
+                    }
+                }
+                Console.WriteLine("Ваша матрица:");
+                for (int i = 0; i < ar.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ar.GetLength(1); j++)
+                    {
+                        Console.CursorLeft = j * (len + 1);
+                        Console.Write(ar[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМатрица не содержит элементов");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(string[][] ar)
+	{
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < ar[i][j].Length)
+                    {
+                        len = ar[i][j].Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;            
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+	}
+    public static void StringLines(string[][] ar)
+    {
+        try
+        {
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.Write(ar[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(int[][] ar)
+    {
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < Convert.ToString(ar[i][j]).Length)
+                    {
+                        len = Convert.ToString(ar[i][j]).Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(double[][] ar)
+    {
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < Convert.ToString(ar[i][j]).Length)
+                    {
+                        len = Convert.ToString(ar[i][j]).Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(decimal[][] ar)
+    {
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < Convert.ToString(ar[i][j]).Length)
+                    {
+                        len = Convert.ToString(ar[i][j]).Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(float[][] ar)
+    {
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < Convert.ToString(ar[i][j]).Length)
+                    {
+                        len = Convert.ToString(ar[i][j]).Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(byte[][] ar)
+    {
+        try
+        {
+            int len = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    if (len < Convert.ToString(ar[i][j]).Length)
+                    {
+                        len = Convert.ToString(ar[i][j]).Length;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * (len + 1);
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void ArrayOfArray(char[][] ar)
+    {
+        try
+        {
+            Console.WriteLine("Ваш массив массивов:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar[i].Length; j++)
+                {
+                    Console.CursorLeft = j * 2;
+                    Console.Write(ar[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив массивов не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(int[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch(NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(double[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(decimal[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(string[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(byte[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(char[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    public static void Array(float[] ar)
+    {
+        try
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        catch (NullReferenceException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\aМассив не содержит ни одного элемента");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+  /*  public static void Vector(vector v)
+    {
+        Console.WriteLine("({0}, {1}, {2})", v.x, v.y, v.z);
+    }*/
+}
---
+++ santaspy/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+namespace santaspy
+{
+    
+    class Program
+    {
+        
+        // Exception pizda = new Exception("Pizda zacyklenaya");
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string[,] names = { { "Vlad", "Nika", "Sasha", "Andrey", "Egor" }, { "", "", "", "", "" } };
+            try
+            {
+                randomize(names);
+            }
+            catch (Exception)
+            {
+                randomize(names);
+            }
+            int x, y;
+            Console.WriteLine("Sosisochka v 3.14");
+            for (int i = 0; i < names.GetLength(1); i++)
+            {
+                x = Console.CursorLeft;
+                y = Console.CursorTop;
+                Console.ReadKey();
+                Console.WriteLine(names[0, i] + " -> " + names[1, i]);
+                Console.WriteLine("Press F to pay respect");
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("**********************************");
+                Console.WriteLine("                                  ");              
+                Console.SetCursorPosition(x, y + 1);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+        }
+        static string[,] randomize(string[,] names)
+        {
+            DateTime dt = DateTime.Now;
+            Random rnd = new Random();
+            int m = 0;
+            Exception pizda = new Exception("Pizda zacyklenaya");
+            int L = names.GetLength(1);
+            int[] ex = new int[L];
+            int k;
+            bool b;
+            for(int i = 0; i < L; i++)
+            {
+                ex[i] = -1;
+            }
+            for (int i = 0; i< L; i++)
+            {
+                m++;
+                if(m > 1000)
+                {
+                    
+                    throw (pizda);
+                }
+                b = false;
+                k = rnd.Next(0, L);
+                for(int j = 0; j<L; j++)
+                {
+                    if(k==ex[j])
+                    {
+                        b = true;
+                    }
+                }
+                if(k==i)
+                {
+                    b = true;
+                }
+                if (!b)
+                {
+                    names[1, i] = names[0, k];
+                    ex[i] = k;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            return names;
+        }
+    }
+}
